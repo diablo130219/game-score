24938536f3a55091c14774cf08f945c1c1dcc5dd
diablo130219 <monicasantoriello1302@gmail.com>
2026-08-12 01:51:25 +0200

    Update online-config.js

--- online-config.js
+++ online-config.js
@@ -9,7 +9,7 @@
   - NON inserire MAI la service_role key.
 */
 window.GS_ONLINE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: "",
-  APP_PUBLIC_URL: ""
+SUPABASE_URL: "https://aflvreaqwzlpzexenutt.supabase.co",
+SUPABASE_PUBLISHABLE_KEY: "sb_publishable_t7umrH9z0sB5QNgIG1hNEQ_7Uh0B0rd",
+APP_PUBLIC_URL: "https://game-score-go9s.onrender.com"
 };
